d8d65f64990ce4a66e8eed93e23267ab973d8c87
vladT <q100km@gmail.com>
2025-06-08 12:13:32 +0500

    fix env

--- src/Data/FireBase.tsx
+++ src/Data/FireBase.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCegoweIk9Xzc0NFEESv2bJKnSQ_ova2Lk',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
   authDomain: 'test-project-f68d4.firebaseapp.com',
   projectId: 'test-project-f68d4',
   storageBucket: 'test-project-f68d4.firebasestorage.app',
   messagingSenderId: '1071644559475',
   appId: '1:1071644559475:web:0a021ab564c83f6a8c0206',
-  databaseURL: 'https://test-project-f68d4-default-rtdb.europe-west1.firebasedatabase.app/',
 }
 
 const app = initializeApp(firebaseConfig)
